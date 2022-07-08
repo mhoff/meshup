c9c88623eaa981d18833d0d5979ad202ad259e1c
Michael Hoff <mail@michael-hoff.net>
2022-07-09 00:01:59 +0200

    Improve connection grid

--- components/connectionGrid.tsx
+++ components/connectionGrid.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import {
-  Table, ActionIcon, Center,
+  Table, Center, Button,
 } from '@mantine/core';
 import { Plus, Minus, Icon } from 'tabler-icons-react';
 import * as React from 'react';
@@ -13,14 +13,17 @@ import {
 
 interface InteractionMode {
   icon: Icon,
+  color: string,
   func: (_: number) => number
 }
 
 const Modes: InteractionMode[] = [{
   icon: Plus,
+  color: 'green',
   func: (v) => v + 1,
 }, {
   icon: Minus,
+  color: 'red',
   func: (v) => v - 1,
 }];
 
@@ -36,24 +39,34 @@ export default function ConnectionGrid() {
             <tr>
               <th>
                 <Center>
-                  <ActionIcon size={16} onClick={() => setMode((mode + 1) % Modes.length)}>
+                  <Button
+                    onClick={() => setMode((mode + 1) % Modes.length)}
+                    color={Modes[mode].color}
+                  >
                     {(() => {
-                      // TODO improve syntax
                       const ModeIcon = Modes[mode].icon;
-                      return (<ModeIcon />);
+                      return (<ModeIcon size={16} />);
                     })()}
-                  </ActionIcon>
+                  </Button>
                 </Center>
               </th>
               {team.members.map((member, index) => (
-                <th key={`col-${member.id}`}>{member.name}</th>
+                <th key={`col-${member.id}`}>
+                  <Center>
+                    {member.name}
+                  </Center>
+                </th>
               ))}
             </tr>
           </thead>
           <tbody>
             {team.members.map((rowMember, rowIndex) => (
               <tr key={`row-${rowMember.id}`}>
-                <td>{rowMember.name}</td>
+                <td>
+                  <Center>
+                    {rowMember.name}
+                  </Center>
+                </td>
                 {team.members.map((colMember, colIndex) => {
                   if (rowIndex === colIndex) {
                     return <td key={`${rowMember.id}/${colMember.id}`} />;
@@ -63,9 +76,14 @@ export default function ConnectionGrid() {
                        jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions */
                     <td
                       key={`${rowMember.id}/${colMember.id}`}
-                      onClick={(e) => setTeam(updateConnectedness(team, rowIndex, colIndex, Modes[mode].func))}
                     >
-                      {getConnection(team, rowIndex, colIndex)}
+                      <Button
+                        fullWidth
+                        variant="outline"
+                        onClick={() => setTeam(updateConnectedness(team, rowIndex, colIndex, Modes[mode].func))}
+                      >
+                        {getConnection(team, rowIndex, colIndex)}
+                      </Button>
                     </td>
                   );
                 })}
